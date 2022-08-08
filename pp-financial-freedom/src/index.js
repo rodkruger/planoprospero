47ccc2b6e6ca9916507551afebbade8f19a750b5
Rodrigo Krüger <rodrigo.kruger.eng@gmail.com>
2022-08-07 22:31:21 -0300

    App first layout

--- pp-financial-freedom/src/index.js
+++ pp-financial-freedom/src/index.js
@@ -1,46 +1,95 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Row } from 'react-bootstrap';
+
 
 class FinancialFreedomForm extends React.Component {
 
   render() {
 
     return (
-      <div>
-        <form>
-          <label>
-            Nome <input name="name" type="text" />
-          </label>
-          <label>
-            Qual é sua idade? <input name="age" type="text" />
-          </label>
-          <label>
-            Qual idade que atingir a Liberdade Financeira: <input name="finFreedomAge" type="text" />
-          </label>
-          <label>
-            Tempo para a Liberdade Financeira <input name="finFreedomTime" type="text" />
-          </label>
-          <label>
-            Expectativa de Vida <input name="lifeExpectancy" type="text" />
-          </label>
-          <label>
-            Tempo desfrutando a Liberdade Financeira <input name="finFreedomTimeUse" type="text" />
-          </label>
-          <label>
-            Renda Mensal pretendida após os 65 anos <input name="monthlyRevenue" type="text" />
-          </label>
-          <label>
-            Taxa de rentabilidade média mensal <input name="rate" type="text" />
-          </label>
-          <label>
-            Qual o patrimônio acumulado aos 65 anos? <input name="patrimony" type="text" />
-          </label>
-          <label>
-            Quanto devo depositar mensalmente? <input name="monthlyAmount" type="text" />
-          </label>
-        </form>
-      </div>
+      <Form className="content">
+        <center>
+          <h1>Intellect AI</h1>
+          <h2>Liberdade Financeira</h2>
+        </center>
+
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column>Qual o seu nome?</Form.Label>
+          <Col>
+            <Form.Control name="name" type="text" size="sm" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column>Qual a sua idade?</Form.Label>
+          <Col>
+            <Form.Control name="age" type="number" size="sm" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column>Qual idade que atingir a Liberdade Financeira</Form.Label>
+          <Col>
+            <Form.Control name="freedomAge" type="number" size="sm" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column>Tempo para a Liberdade Financeira</Form.Label>
+          <Col>
+            <Form.Control id="freedomTime" name="finFreedomTime" type="number" disabled />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column>Expectativa de Vida</Form.Label>
+          <Col>
+            <Form.Control name="lifeExpectancy" type="number" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column>Tempo desfrutando a Liberdade Financeira</Form.Label>
+          <Col>
+            <Form.Control name="finFreedomTimeUse" type="number" disabled />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column>Renda Mensal pretendida após os 65 anos</Form.Label>
+          <Col>
+            <Form.Control name="monthlyRevenue" type="number" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column>Taxa de rentabilidade média mensal</Form.Label>
+          <Col>
+            <Form.Control name="rate" type="number" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column>Qual o patrimônio acumulado aos 65 anos?</Form.Label>
+          <Col>
+            <Form.Control name="patrimony" type="number" disabled />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column>Quanto devo depositar mensalmente?</Form.Label>
+          <Col>
+            <Form.Control name="monthlyAmount" type="number" disabled />
+          </Col>
+        </Form.Group>
+
+      </Form >
     );
   }
 }
